b1865c728334bb50d5c76307e9dcab29a901b5f7
Andrew Aliaj <andyaliaj@gmail.com>
2024-05-06 16:49:52 -0400

    first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const isLoggedIn = false;
 
+  const log=true;
+let Pet=false;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="text-center text-5xl">
+    <h1> Hello App</h1>
+    <>
+    {Pet ? <p> Cat</p> : <p> Dog</p>}
+    </>
+    <div>
+      {isLoggedIn ? <h1>Welcome back!</h1> : <h1>Please log in.</h1>}
+        </div>
+        <div>
+
+          {log ? <h1> Name please</h1> : <h1> Please log in.</h1>}
+        </div>
+    </div>
     </>
   )
 }
